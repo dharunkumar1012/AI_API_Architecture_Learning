6d40eda8730996bf740e6c98a62c81a60a039ba2
Dharun Kumar <dharunkumar6381@gmail.com>
2026-09-22 13:43:05 +0530

    Status_code completed

--- status_code.py
+++ status_code.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -12,6 +12,21 @@ class UserResponse(BaseModel):
     name: str
     email: str
 
+class ProductRequest(BaseModel):
+    name: str
+    quantity: int
+
+# Status code - 201
 @app.post("/users", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 def create_user(user: UserRequest):
     return user
+
+# Status code - 400
+@app.post("/products")
+def create_product(product: ProductRequest):
+    if product.quantity > 10:
+        raise HTTPException(
+            status_code= status.HTTP_400_BAD_REQUEST,
+            detail="Quantity can't be greater than 10"
+        )
+    return product
